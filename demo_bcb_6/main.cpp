70b641bc4d6d94bd6c7df81dfece785fa622891f
Maxim verzhak Akinin <verzhak@gmail.com>
2013-11-21 12:40:19 +0400

    TBitmap : 10 %

--- demo_bcb_6/main.cpp
+++ demo_bcb_6/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 #pragma argsused
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fwdreason, LPVOID lpvReserved)
 {
-        return 1;
+	return 1;
 }
 
 image src, dst;
@@ -22,7 +22,59 @@ char * in, * out;
 
 bool is_correct_index(const bool is_param, const unsigned ind)
 {
-        return ind < 2;
+	return ind < 2;
+}
+
+Graphics::TBitmap * matrix_to_TBitmap(const matrix * mtx)
+{
+	Graphics::TBitmap * img = NULL;
+
+	try
+	{
+		unsigned height, width, ch_num;
+		int elem_type;
+
+		throw_if(matrix_height(mtx, & height));
+		throw_if(matrix_width(mtx, & width));
+		throw_if(matrix_number_of_channel(mtx, & ch_num));
+		throw_if(matrix_element_type(mtx, & elem_type));
+		throw_null(img = new Graphics::TBitmap());
+
+		// TODO
+	}
+	catch(...)
+	{
+		if(img != NULL)
+			delete img;
+
+		img = NULL;
+	}
+
+	return img;
+}
+
+matrix TBitmap_to_matrix(const Graphics::TBitmap * img)
+{
+	matrix mtx = NULL;
+
+	try
+	{
+		const unsigned height, width, ch_num; // TODO
+		const int elem_type; // TODO
+
+		throw_null(mtx = matrix_create(height, width, ch_num, elem_type));
+
+		// TODO
+	}
+	catch(...)
+	{
+		if(mtx != NULL)
+			matrix_delete(mtx);
+
+		mtx = NULL;
+	}
+
+	return mtx;
 }
 
 // ############################################################################ 
@@ -31,8 +83,8 @@ int EXPORT_FUNCTION init(char * module_name, const unsigned module_name_len, uns
 {
 	src = NULL;
 	dst = NULL;
-        in = NULL;
-        out = NULL;
+	in = NULL;
+	out = NULL;
 
 	strncpy(module_name, "demo_bcb_6", module_name_len);
 	module_name[module_name_len - 1] = '\0';
@@ -156,12 +208,12 @@ int EXPORT_FUNCTION set_value(const bool is_param, const unsigned ind, const voi
 
 int EXPORT_FUNCTION run()
 {
-        dst = image_create(src->height, src->width, src->ch_num);
+	dst = image_create(src->height, src->width, src->ch_num);
 
-        printf("In module: %s\n", in);
+	printf("In module: %s\n", in);
 
-        if(out == NULL)
-                out = strdup("Output string");
+	if(out == NULL)
+		out = strdup("Output string");
 
 	return 0;
 }
